5aa20931e77ca3dcd2049eb4412dc2d3e0431ab5
André "decko" de Brito <brito.afa@gmail.com>
2019-04-25 16:00:04 -0300

    Test the API URL and namespace. Tests failing

--- core/test_core.py
+++ core/test_core.py
@@ -1,9 +1,10 @@
 from django.urls import resolve
+from rest_framework import status
 
 
 def test_telephone_endpoint_return_200_OK(client):
     """
-    Test if root api url return 200 OK Http Status.
+    Test if root url(/) return 200 OK HTTP Status.
     """
 
     request = client.get('/')
@@ -13,7 +14,7 @@ def test_telephone_endpoint_return_200_OK(client):
 
 def test_telephone_endpoint_namespace_definition(client):
     """
-    Test if the namespace of root api url is defined as "core" and "index"
+    Test if the namespace of root url(/) is defined as "core" and "index"
     as his name.
     """
 
@@ -22,3 +23,27 @@ def test_telephone_endpoint_namespace_definition(client):
 
     assert resolved.namespace == "core"
     assert resolved.url_name == "index"
+
+
+def test_telephone_api_endpoint(client):
+    """
+    Test if root api url(/api/v1/) return 200 Ok HTTP Status.
+    """
+
+    url = '/api/v1/'
+    request = client.get(url)
+
+    assert request.status_code == status.HTTP_200_OK
+
+
+def test_telephone_api_namespace_definition(client):
+    """
+    Test if the namespace of root api url(/api/v1/) is defined as "core" and
+    "api" as his name.
+    """
+
+    url = '/api/v1/'
+    resolved = resolve(url)
+
+    assert resolved.namespace == "core"
+    assert resolved.url_name == "api"
